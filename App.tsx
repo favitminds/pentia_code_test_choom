8a6e4b92acfef1fb58f474ff154a91fcdb43d16b
favitminds <fav@it-minds.dk>
2023-01-30 13:47:39 +0100

    Moved navigation to seperate component

--- App.tsx
+++ App.tsx
@@ -1,18 +1,8 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {HomeScreen} from './src/components/HomeScreen';
-
-const Stack = createNativeStackNavigator();
+import Navigation from './src/navigation/Navigation';
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Navigation />;
 }
 
 export default App;
