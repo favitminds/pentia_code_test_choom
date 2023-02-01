91be4a2c54092fd6dbf5147cf22f55472f0754d1
favitminds <fav@it-minds.dk>
2023-02-01 18:06:00 +0100

    navigation changes for getting params from navigation hooks

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -5,8 +5,19 @@ import {useContext} from 'react';
 import {AuthenticationScreen} from '../screens/AuthenticationScreen';
 import {ChatRoomsScreen} from '../screens/ChatRoomsScreen';
 import {ChatRoomScreen} from '../screens/ChatRoomScreen';
+import {
+  SCREEN_NAME_CHAT_ROOM,
+  SCREEN_NAME_CHAT_ROOMS_LIST,
+  SCREEN_NAME_SIGN_UP
+} from '../utils/globals';
 
-const Stack = createNativeStackNavigator();
+export type StackParamList = {
+  Choom: undefined;
+  SignUp: undefined;
+  ChatRoom: {id: string; name: string};
+};
+
+const Stack = createNativeStackNavigator<StackParamList>();
 
 export const Navigation = () => {
   const {user} = useContext(AuthenticationContext);
@@ -14,15 +25,19 @@ export const Navigation = () => {
     <NavigationContainer>
       <Stack.Navigator>
         {user ? (
-          <Stack.Screen name="Choom" component={ChatRoomsScreen} />
+          <Stack.Screen name={SCREEN_NAME_CHAT_ROOMS_LIST} component={ChatRoomsScreen} />
         ) : (
           <Stack.Screen
-            name="Sign Up"
+            name={SCREEN_NAME_SIGN_UP}
             options={{headerShown: false}}
             component={AuthenticationScreen}
           />
         )}
-        <Stack.Screen name="ChatRoom" component={ChatRoomScreen} />
+        <Stack.Screen
+          name={SCREEN_NAME_CHAT_ROOM}
+          component={ChatRoomScreen}
+          options={({route}) => ({title: route.params.name})}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
