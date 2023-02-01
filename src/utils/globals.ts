8e0d2a16273600ea031a73e74594e2ea1cc6607c
favitminds <fav@it-minds.dk>
2023-02-01 18:03:27 +0100

    added globals

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,1 +1,7 @@
 export const DB_CHAT_ROOMS_COLLECTION_NAME = 'chat-rooms';
+export const DB_CHAT_MESSAGES_COLLECTION_NAME = 'chat-messages';
+export const SCREEN_NAME_CHAT_ROOM = 'ChatRoom';
+export const SCREEN_NAME_CHAT_ROOMS_LIST = 'Choom';
+export const SCREEN_NAME_SIGN_UP = 'SignUp';
+export const MESSAGES_LIMIT = 50;
+export const MESSAGES_UPDATE_LIMIT = 1;
