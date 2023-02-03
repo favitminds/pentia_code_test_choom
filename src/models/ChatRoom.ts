f66551662d4532a24bddf656ae32dd663e7c9945
favitminds <fav@it-minds.dk>
2023-02-03 16:09:33 +0100

    cleanup and updatedAt field added to chatrooms

--- src/models/ChatRoom.ts
+++ src/models/ChatRoom.ts
@@ -1,19 +1,6 @@
-import {FirebaseFirestoreTypes} from '@react-native-firebase/firestore';
-
 export type ChatRoom = {
-  // TODO: last edited, from firestore?
-  // TODO: date created, from firestore?
   id: string;
   name: string;
   description: string;
-};
-
-export const mapToChatRoomModel = (
-  firestoreDocument: FirebaseFirestoreTypes.QueryDocumentSnapshot<FirebaseFirestoreTypes.DocumentData>
-): ChatRoom => {
-  return {
-    id: firestoreDocument.id,
-    description: firestoreDocument.data().description,
-    name: firestoreDocument.data().name
-  };
+  updatedAt: Date;
 };
--- src/services/ChatRoomService.ts
+++ src/services/ChatRoomService.ts
@@ -1,9 +1,28 @@
-import firestore from '@react-native-firebase/firestore';
-import {mapToChatRoomModel} from '../models/ChatRoom';
+import firestore, {FirebaseFirestoreTypes} from '@react-native-firebase/firestore';
+import {ChatRoom} from '../models/ChatRoom';
 import {DB_CHAT_ROOMS_COLLECTION_NAME} from '../utils/globals';
 
 export const getChatRooms = async () => {
-  const data = await firestore().collection(DB_CHAT_ROOMS_COLLECTION_NAME).get();
+  const data = await firestore()
+    .collection(DB_CHAT_ROOMS_COLLECTION_NAME)
+    .orderBy('updatedAt', 'desc')
+    .get();
 
   return data.docs.map(doc => mapToChatRoomModel(doc));
+};
+
+export const updateEditTimeOfChatRoom = async (chatRoomId: string, updatedAt: Date) => {
+  await firestore().collection(DB_CHAT_ROOMS_COLLECTION_NAME).doc(chatRoomId).update({updatedAt});
+};
+
+const mapToChatRoomModel = (
+  firestoreDocument: FirebaseFirestoreTypes.QueryDocumentSnapshot<FirebaseFirestoreTypes.DocumentData>
+): ChatRoom => {
+  const data = firestoreDocument.data();
+  return {
+    id: firestoreDocument.id,
+    description: data.description,
+    name: data.name,
+    updatedAt: data.updatedAt.toDate()
+  };
 };
