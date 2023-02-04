fdf7ec9ff90c5b1e1489be696e6c4ca922bf0d55
favitminds <fav@it-minds.dk>
2023-02-04 16:35:41 +0100

    added colors for app

---
+++ src/theme/colors.ts
@@ -0,0 +1,22 @@
+export const colors = {
+  ui: {
+    primary: '#5241BA'
+  },
+  bg: {
+    primary: '#787DFF'
+  },
+  chatRoom: {
+    bg: {
+      primary: '#5362EC'
+    }
+  },
+  chatMessage: {
+    bg: {
+      currentUser: '#30398F',
+      otherUsers: '#3D49B3'
+    }
+  },
+  text: {
+    primary: '#FFFFFF'
+  }
+};
